d94b105124c8f943d1d83d30df3bbb0f17c44862
NandhiniKrishnamoorthy <nandhiniprasath25@gmail.com>
2021-09-17 08:58:16 -0700

    Docs: Updated url

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ var errorMsg = document.querySelector("#errorMessage");
 
 var serverUrl = "https://api.funtranslations.com/translate/numbers.json";
 
-function constr(text){
+function constructUrl(text){
     var url = serverUrl + "?" + "text=" + text;
     return url;
 }
 
 function errorHandler(event) {
-    errorMsg.innerText = "Sorry! You can convert only 5 times in 1 hour, Please try after sometime"
+    errorMsg.innerText = "Sorry! You can convert only 5 times in 1 hour, Please try after sometime";
+    outputValue.innerText = "";
 }
 
 buttonConvert.addEventListener("click", function numberConvertion() {
-    fetch(constr(inputValue.value))
+    fetch(constructUrl(inputValue.value))
         .then(reponse => response.json)
         .then(json => {
             outputValue.innnerText = json.contents.translated;
