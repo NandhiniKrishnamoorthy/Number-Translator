556c06b5459efd7bbc0f05323ca9296c59b4dca6
NandhiniKrishnamoorthy <nandhiniprasath25@gmail.com>
2021-09-18 18:58:29 -0700

    Docs: responsive viewport

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-/*var buttonConvert = document.querySelector("#btnConvert");
-var inputValue = document.querySelector("#inputArea");
-var outputValue = document.querySelector("#outputArea");
-var errorMsg = document.querySelector("#errorMessage");
-
-function constructUrl(text) {
-    var serverUrl = "https://api.funtranslations.com/translate/numbers.json";
-    var url = serverUrl + "?" + "text=" + text;
-    return url;
-}
-
-function errorHandler(event) {
-    errorMsg.innerText = "Sorry! You can convert only 5 times in 1 hour, Please try after sometime";
-    outputValue.innerText = "";
-}
-
-buttonConvert.addEventListener("click", function numberConvertion() {
-    fetch(constructUrl(inputValue.value))
-        .then(response => response.json())
-        .then(json => {
-            outputValue.innerText = json.contents.translated;
-        })
-        .catch(errorHandler)
-})*/
-
 var buttontrans = document.querySelector("#btntranslate");
 var txtinputarea = document.querySelector("#txtarea");
 var outputtextarea = document.querySelector("#outputtext");
