a423ca595211b08ba8b8d42a3382c12ca8227ab9
NandhiniKrishnamoorthy <nandhiniprasath25@gmail.com>
2021-09-18 18:54:12 -0700

    Docs: responsive viewport

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var buttonConvert = document.querySelector("#btnConvert");
+/*var buttonConvert = document.querySelector("#btnConvert");
 var inputValue = document.querySelector("#inputArea");
 var outputValue = document.querySelector("#outputArea");
 var errorMsg = document.querySelector("#errorMessage");
@@ -21,4 +21,32 @@ buttonConvert.addEventListener("click", function numberConvertion() {
             outputValue.innerText = json.contents.translated;
         })
         .catch(errorHandler)
+})*/
+
+var buttontrans = document.querySelector("#btntranslate");
+var txtinputarea = document.querySelector("#txtarea");
+var outputtextarea = document.querySelector("#outputtext");
+var errortext = document.querySelector(".displayerror");
+
+var serverURL = "https://api.funtranslations.com/translate/numbers.json";
+
+function findurl(inputtext) {
+    var url = serverURL + "?" + "text=" + inputtext;
+    return url;
+}
+
+function errorHandling(error) {
+    //console.log("Some ERROR Occured!")
+    errortext.innerText = "You can use this for only 5 times per hour, please try after 1 hour";
+    outputtextarea.innerText = "";
+}
+
+buttontrans.addEventListener("click", function clickEvent() {
+    text = txtinputarea.value;
+    fetch(findurl(text))
+        .then(response => response.json())
+        .then(json => {
+            outputtextarea.innerText = json.contents.translated;
+        })
+        .catch(errorHandling)
 })
